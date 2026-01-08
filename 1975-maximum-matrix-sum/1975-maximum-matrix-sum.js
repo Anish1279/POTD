a83297c55a0c80eb1abd32be8e9dd7984e200db4
Anish1279 <117334096+Anish1279@users.noreply.github.com>
2026-01-09 00:20:51 +0530

    Time: 16 ms (10.85%), Space: 67.1 MB (23.23%) - LeetHub

---
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.js
@@ -0,0 +1,16 @@
+var maxMatrixSum = function(matrix) {
+    let totalSum = 0;
+    let neg = 0;
+    let minAbs = Infinity;
+
+    for (const row of matrix) {
+        for (const v of row) {
+            if (v < 0) neg++;
+            const av = Math.abs(v);
+            totalSum += av;
+            minAbs = Math.min(minAbs, av);
+        }
+    }
+
+    return neg % 2 === 0 ? totalSum : totalSum - 2 * minAbs;
+};
