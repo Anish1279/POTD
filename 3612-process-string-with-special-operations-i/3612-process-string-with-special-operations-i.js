21dc33dd4cd6f018f740a3d612ea3b427ba8ebed
Anish1279 <117334096+Anish1279@users.noreply.github.com>
2026-06-17 00:00:19 +0530

    Time: 61 ms (51.85%), Space: 81.5 MB (40.74%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+function processStr(s) {
+    let result = [];
+    for (const ch of s) {
+        if (ch === "*") {
+            if (result.length) result.pop();
+        } else if (ch === "#") {
+            result = result.concat(result);
+        } else if (ch === "%") {
+            result.reverse();
+        } else {
+            result.push(ch);
+        }
+    }
+    return result.join("");
+}
